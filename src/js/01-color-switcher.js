bd49f2716f4840c8e7579c31f0a6bea74cbfda0d
adrianblazejczyk <121712995+adrianblazejczyk@users.noreply.github.com>
2023-06-24 15:35:24 +0200

    firs taxk - end

--- src/js/01-color-switcher.js
+++ src/js/01-color-switcher.js
@@ -1,1 +1,39 @@
 
+/*-------------------------------------------------------------------------------------------------------------------------------- */
+const btnStart = document.querySelector(`[data-start]`);
+const btnStop = document.querySelector(`[data-stop]`);
+let timerId = null;
+
+/*-------------------------------------------------------------------------------------------------------------------------------- */
+function getRandomHexColor() {
+    return `#${Math.floor(Math.random() * 16777215).toString(16)}`;
+}
+
+function stopChangeColor(){
+    btnStop.setAttribute(`disabled`,"");
+    btnStart.removeAttribute(`disabled`);
+    clearInterval(timerId);
+} 
+function changeBgc(){
+    document.body.style.backgroundColor = getRandomHexColor();
+}
+function startChangeColor(){
+    btnStop.removeAttribute(`disabled`); 
+    btnStart.setAttribute(`disabled`,"");
+    changeBgc();
+    timerId = setInterval(() => {
+        changeBgc()
+    }, 1000);
+}
+/*-------------------------------------------------------------------------------------------------------------------------------- */
+btnStop.setAttribute(`disabled`, ""); 
+const btnStartLisner = btnStart.addEventListener('click', startChangeColor);
+const btnStopLisner = btnStop.addEventListener('click', stopChangeColor);
+
+
+
+
+
+
+
+
