ded6dbc9cbd9ae1824d13edff917e6aa0e1f8af1
adrianblazejczyk <121712995+adrianblazejczyk@users.noreply.github.com>
2023-06-25 18:54:47 +0200

    task 3 - end

--- src/js/01-color-switcher.js
+++ src/js/01-color-switcher.js
@@ -1,4 +1,3 @@
-
 /*-------------------------------------------------------------------------------------------------------------------------------- */
 const btnStart = document.querySelector(`[data-start]`);
 const btnStop = document.querySelector(`[data-stop]`);
@@ -6,34 +5,26 @@ let timerId = null;
 
 /*-------------------------------------------------------------------------------------------------------------------------------- */
 function getRandomHexColor() {
-    return `#${Math.floor(Math.random() * 16777215).toString(16)}`;
+  return `#${Math.floor(Math.random() * 16777215).toString(16)}`;
 }
 
-function stopChangeColor(){
-    btnStop.setAttribute(`disabled`,"");
-    btnStart.removeAttribute(`disabled`);
-    clearInterval(timerId);
-} 
-function changeBgc(){
-    document.body.style.backgroundColor = getRandomHexColor();
+function stopChangeColor() {
+  btnStop.setAttribute(`disabled`, '');
+  btnStart.removeAttribute(`disabled`);
+  clearInterval(timerId);
 }
-function startChangeColor(){
-    btnStop.removeAttribute(`disabled`); 
-    btnStart.setAttribute(`disabled`,"");
+function changeBgc() {
+  document.body.style.backgroundColor = getRandomHexColor();
+}
+function startChangeColor() {
+  btnStop.removeAttribute(`disabled`);
+  btnStart.setAttribute(`disabled`, '');
+  changeBgc();
+  timerId = setInterval(() => {
     changeBgc();
-    timerId = setInterval(() => {
-        changeBgc()
-    }, 1000);
+  }, 1000);
 }
 /*-------------------------------------------------------------------------------------------------------------------------------- */
-btnStop.setAttribute(`disabled`, ""); 
+btnStop.setAttribute(`disabled`, '');
 const btnStartLisner = btnStart.addEventListener('click', startChangeColor);
 const btnStopLisner = btnStop.addEventListener('click', stopChangeColor);
-
-
-
-
-
-
-
-
--- src/js/03-promises.js
+++ src/js/03-promises.js
@@ -1,8 +1,44 @@
+/*---------------------------------------------------------------------------------------------------------------- */
+import Notiflix from 'notiflix';
+/*---------------------------------------------------------------------------------------------------------------- */
 function createPromise(position, delay) {
-  const shouldResolve = Math.random() > 0.3;
-  if (shouldResolve) {
-    // Fulfill
-  } else {
-    // Reject
+  return new Promise((resolve, reject) => {
+    setTimeout(() => {
+      const shouldResolve = Math.random() > 0.3;
+      const obiekt = {
+        position: position,
+        delay: delay,
+      };
+      if (shouldResolve) {
+        resolve(obiekt);
+      } else {
+        reject(obiekt);
+      }
+    }, delay);
+  });
+}
+
+function submitForm(eve) {
+  eve.preventDefault();
+  const delay = Number(formWraper.delay.value);
+  const delayStep = Number(formWraper.step.value);
+  const amout = Number(formWraper.amount.value);
+
+  for (let index = 0; index < amout; index++) {
+    createPromise(index + 1, delay + delayStep * index)
+      .then(obiekt => {
+        Notiflix.Notify.success(
+          `Fulfilled promise ${obiekt.position} in ${obiekt.delay} ms`
+        );
+      })
+      .catch(obiekt => {
+        Notiflix.Notify.failure(
+          `Rejected promise ${obiekt.position} in ${obiekt.delay} ms`
+        );
+      });
   }
 }
+/*---------------------------------------------------------------------------------------------------------------- */
+const bntSubmit = document.querySelector('[type=submit]');
+const bntSubmitListener = bntSubmit.addEventListener('click', submitForm);
+const formWraper = document.querySelector('.form');
